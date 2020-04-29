c58983f7188788573704301c23d181cde7b4c029
JessPergentino <jessicapergentino@hotmail.com>
2020-04-29 18:47:49 -0300

    teste ngrok

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { get } from 'idb-keyval'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'http://b10f335b.sa.ngrok.io'
 })
 
 api.interceptors.request.use(async config => {
